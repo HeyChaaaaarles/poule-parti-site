87f171857977b54abdbd897bc07009eb6f95dacf
HeyChaaaaarles <la229157@student.helha.be>
2026-02-11 15:00:20 +0000

    Initial commit: Séparation HTML, CSS et JS

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('search-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Empêche l'envoi classique du formulaire
+    window.location.href = 'https://www.youtube.com./watch?v=dQw4w9WgXcQ';
+});
