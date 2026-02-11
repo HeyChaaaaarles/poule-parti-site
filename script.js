4d5970c1886505795baa7ec56ab49dfe10960612
HeyChaaaaarles <la229157@student.helha.be>
2026-02-11 17:44:34 +0100

    Test Final Webhook

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 document.getElementById('search-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Empêche l'envoi classique du formulaire
+    event.preventDefault();
     window.location.href = 'https://www.youtube.com./watch?v=dQw4w9WgXcQ';
 });
+const themeToggle = document.getElementById('theme-toggle');
+
+themeToggle.addEventListener('change', () => {
+    document.body.classList.toggle('dark-mode');
+
+    if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+});
+
+if (localStorage.getItem('theme') === 'dark') {
+    themeToggle.checked = true;
+    document.body.classList.add('dark-mode');
+}
