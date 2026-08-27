dd4ecd79a29321fc337a25291379fcca224efb2c
HeyChaaaaarles <la229157@student.helha.be>
2026-08-27 22:25:19 +0200

    Rename graphana.html to home.html, add login page with styles and scripts

---
+++ public/troll-grafana/scripts/login.js
@@ -0,0 +1,9 @@
+document.getElementById('forgot-pass').addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+});
+
+document.querySelector('.login-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    window.location.href = 'home.html';
+});
